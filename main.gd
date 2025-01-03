9876e3c5e08bf3e5496e2433e7c1d90bda63a108
Yulya-S <yulya_Shabsnova_03@mail.ru>
2025-01-03 18:23:26 +0800

    added system rotation

--- main.gd
+++ main.gd
@@ -1,19 +1,45 @@
 extends Node3D
 @onready var CameraNode = $CameraNode/Camera3D/CanvasLayer
-const base_camera_rotation = -( 90 - 1.57079637050629 ) # разница между предполагаемым и реальным поворотом камеры
+var plane = 0 # 0 - x, 1 - y
+var turning_speed = 5 # значение на которое изменяется поворот при прокрутке колесика мыши
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# изначальный поворот камеры над системой
+	$CameraNode.rotation_degrees.x = 270
+	$CameraNode.rotation_degrees.y = 0
+	CameraNode.get_child(4).text = "x: " + str($CameraNode.rotation_degrees.x)\
+									+ ", y: " + str($CameraNode.rotation_degrees.y)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action("mouse_wheel_up"):
+		$CameraNode.rotation_degrees[plane] += turning_speed
+		if $CameraNode.rotation_degrees[plane] > 360:
+			$CameraNode.rotation_degrees[plane] = 0
+	elif event.is_action("mouse_wheel_down"):
+		$CameraNode.rotation_degrees[plane] -=  turning_speed
+		if $CameraNode.rotation_degrees[plane] < 0:
+			$CameraNode.rotation_degrees[plane] = 360
+	CameraNode.get_child(4).text = "x: " + str($CameraNode.rotation_degrees.x)\
+									+ ", y: " + str($CameraNode.rotation_degrees.y)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var fps = Performance.get_monitor(Performance.TIME_FPS)
 	CameraNode.get_child(0).text = "FPS: " + str(fps)
 	CameraNode.get_child(1).get_child(0).text = str(CameraNode.get_child(1).value)
 	CameraNode.get_child(2).get_child(0).text = str(CameraNode.get_child(2).value)
 
-func _on_v_slider_value_changed(value: float) -> void:
-	$CameraNode.rotation.x = -90 - base_camera_rotation #+ 88.42920362949371
-	#print(value)
+# переключение
+func _on_button_button_down() -> void:
+	match plane:
+		0:
+			plane = 1
+			$CameraNode/Camera3D/CanvasLayer/Button.text = "Y"
+		1:
+			plane = 0
+			$CameraNode/Camera3D/CanvasLayer/Button.text = "X"
+
+
+func _on_center_button_down() -> void:
+	$CameraNode.rotation_degrees.x = 270
+	$CameraNode.rotation_degrees.y = 0
+	
--- objects/objects.gd
+++ objects/objects.gd
@@ -1,27 +1,35 @@
 extends Node3D
 
-var radius = 100
-var t = 0.0
-var speed_multiplier = 1
-var speed = 0.0
+var radius = 100 # растояние от центра
+var t = 0.0 # текущий поворот
+var speed_multiplier = 1 # множитель скорости движения
+var speed = 0.0 # скорость движения объекта
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	radius = randi_range(50, 200)
-	t = randf_range(0.0, 2.3)
-	speed = randf_range(0.001, 0.01)
+	calculation_parameters()
+
+# расчеты параметров
+func calculation_parameters(new_radius: int = -1):
+	if new_radius < 0:
+		radius = randi_range(50, 200)
+		t = randf_range(0.0, 2.3)
+		speed = randf_range(0.001, 0.01)
+	else:
+		pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# расчеты по формулам
 	var x = radius * cos(t) + radius
 	var y = radius * sin(t) + radius
 	var x1 = radius * cos(t + 0.1) + radius
 	var y1 = radius * sin(t + 0.1) + radius
 	
+	# изменение поворота объекта
 	t += speed_multiplier * speed
 	if t > 2 * PI:
 		t = 0
 	
+	# применение новой позиции относительно старой
 	position.x = x1 - x
 	position.z = y1 - y
